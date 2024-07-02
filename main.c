d0b1561409e25319104a1e23d67e1c28d0c11912
Ubuntu <ubuntu@ip-172-31-1-44.eu-north-1.compute.internal>
2024-07-02 12:17:04 +0000

    commit made in feature

--- main.c
+++ main.c
@@ -3,13 +3,13 @@ pseudo code
 Initial Function()
 {
 
- this  added in feature2
+ this  added in feature2 by feature2
 }
 
 Initial Security()
 {
 
- this is added in feature2
+ this is added in feature2 by feature2
 }
 
 
